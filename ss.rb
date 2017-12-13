6234ba898fedbf99d6bdf5b0e9bf101e8da2aabd
Finn Bacall <fbacall@users.noreply.github.com>
2017-12-13 11:00:17 +0000

    Use string interpolation

--- ss.rb
+++ ss.rb
@@ -1,1 +1,1 @@
-gets.split(',').map(&:strip).shuffle.tap { |names| ((names += [names.first]).length - 1).times { |i| File.write(names[i] + '.txt', "#{names[i+1]}\n") } }
+gets.split(',').map(&:strip).shuffle.tap { |names| ((names += [names.first]).length - 1).times { |i| File.write("#{names[i]}.txt", "#{names[i+1]}\n") } }
