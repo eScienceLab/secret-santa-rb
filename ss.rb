1c184ddc0e00bbd387ff099c1f4cffc626efcf08
Finn Bacall <fbacall@users.noreply.github.com>
2017-12-13 10:48:07 +0000

    1 line

--- ss.rb
+++ ss.rb
@@ -1,2 +1,1 @@
-names = gets.split(',').map(&:strip).shuffle
-((names += [names.first]).length - 1).times { |i| File.write(names[i] + '.txt', "#{names[i+1]}\n") }
+gets.split(',').map(&:strip).shuffle.tap { |names| ((names += [names.first]).length - 1).times { |i| File.write(names[i] + '.txt', "#{names[i+1]}\n") } }
