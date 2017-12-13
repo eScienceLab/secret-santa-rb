e4af4470c74ab4722c5ed7ad84f855a0c7d322e8
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-12-13 11:11:45 +0000

    Use zip and rotate to generate a list of pairs

--- ss.rb
+++ ss.rb
@@ -1,1 +1,1 @@
-gets.split(',').map(&:strip).shuffle.tap { |names| ((names += [names.first]).length - 1).times { |i| File.write("#{names[i]}.txt", "#{names[i+1]}\n") } }
+gets.split(',').map(&:strip).shuffle.tap { |names| names.zip(names.rotate) }.each { |giver, receiver| File.write("#{giver}.txt", "#{receiver}\n") } }
