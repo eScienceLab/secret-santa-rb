3555c401eb6f72b5c21b7dc970d4d6b890ed2265
Finn Bacall <fbacall@users.noreply.github.com>
2017-12-13 21:38:11 +0000

    Fixed typo

--- ss.rb
+++ ss.rb
@@ -1,1 +1,1 @@
-gets.split(',').map(&:strip).shuffle.tap { |names| names.zip(names.rotate) }.each { |giver, receiver| File.write("#{giver}.txt", "#{receiver}\n") } }
+gets.split(',').map(&:strip).shuffle.tap { |names| names.zip(names.rotate).each { |giver, receiver| File.write("#{giver}.txt", "#{receiver}\n") } }
